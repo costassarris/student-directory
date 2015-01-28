cdecbb5c45143677029c1af0979f34a01d06695b
Costas Sarris <costassarris@gmail.com>
2015-01-28 13:20:09 +0000

    Added first comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "-----------------"
 puts "Dr. Hannible Lecter"
@@ -9,7 +10,7 @@ puts "The Alien"
 puts "Terminator"
 puts "Freddy Kruger"
 puts "The Joker"
-
+#it's important that print() doesn't add new line characters
 print "Overall, we have "
 print 9
 puts " great students"
