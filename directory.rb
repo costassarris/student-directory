cb9ff05397dd61e206c526937c7b6a115a43d868
Costas Sarris <costassarris@gmail.com>
2015-01-29 17:36:47 +0000

    Better aligned text using center

--- directory.rb
+++ directory.rb
@@ -1,7 +1,8 @@
 #lets put all students into an array
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	puts ""
+	puts "Please enter the names of the students".center(60)
+	puts "To finish, just hit return twice".center(60)
 	#create an empty array
 	students = []
 	#get the first name
@@ -10,11 +11,11 @@ def input_students
 	#while the name is not empty, repeat this code
 	while !name.empty? do
 		#ask user for students country of birth
-		puts "Country of birth?"
+		puts "Country of birth?".center(60)
 		cob = gets.chomp
 		#add the student hash to the array
 		students << {:name => name, :cohort => :February, :cob => cob}
-		puts "Now we have #{students.length} students"
+		puts "Now we have #{students.length} students".center(60)
 		#get another name from the user
 		name = gets.chomp
 	end
@@ -23,8 +24,8 @@ def input_students
 end
 
 def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "-----------------"
+	puts "The students of my cohort at Makers Academy".center(60)
+	puts "-----------------".center(60)
 end
 
 def print(students)
